d6c64853b494d08b2c69ac6b2aa2f0858e06c673
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-03-18 15:32:57 -0400

    commit tree 37eb6361f182b7ebfe5aa8fd11ae23b2ca35fe94

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker(name) 
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+    attendees.map do |attendee| 
+       "Hello, my name is #{attendee}."
+    end
+end
+
+def assign_rooms(attendees)
+    attendees.map.with_index(1) do |attendee, room_num|
+    "Hello, #{attendee}! You'll be assigned to room #{room_num}!"
+    end
+end
